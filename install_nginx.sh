643109d100626c7e3c9c4ce4c1af84672e5d75ae
Siddesh K T <ktsiddeshwaraswamy@gmail.com>
2026-08-04 00:12:02 +0000

    Feat:Added shell script for NGINX

---
+++ install_nginx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this is a script to install and enable nginx
+# this is to show feat and chore commit messgae
+sudo apt-get update -y
+sudo apt install nginx -y
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+echo "NGINX Installed"
